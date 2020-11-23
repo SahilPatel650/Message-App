eaac6d1f426cd62f67702346b1ef7116264bac09
Sahil <sahil.va.us@gmail.com>
2020-11-23 16:50:02 -0500

    dependencies installed

--- Message/Resources/AppDelegate.swift
+++ Message/Resources/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        FirebaseApp.configure()
+            return true
     }
 
     // MARK: UISceneSession Lifecycle
--- Podfile
+++ Podfile
@@ -4,16 +4,19 @@
 target 'Message' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+
+
 
   # Pods for Message
 
   target 'MessageTests' do
     inherit! :search_paths
-    # Pods for testing
   end
 
   target 'MessageUITests' do
-    # Pods for testing
   end
 
 end
